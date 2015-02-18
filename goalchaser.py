55fe4f85e9f28d65b86002179a8f96d138b88619
yipyip <dijuek@googlemail.com>
2015-02-18 12:42:27 +0100

    frame duration title

--- goalchaser.py
+++ goalchaser.py
@@ -94,7 +94,6 @@ class PygView(object):
 
         pyg.init()
         self.canvas = pyg.display.set_mode((self.width, self.height), pyg.DOUBLEBUF)
-        pyg.display.set_caption("Press ESC to exit.")
         self.clock = pyg.time.Clock()
 
 
@@ -110,6 +109,8 @@ class PygView(object):
         """
         running = True
         while running:
+            pyg.display.set_caption("Press ESC to exit."
+                                    "{}{:2.5f}".format(" " * 8, self.frame_duration_secs))
             self.clock.tick_busy_loop(self.fps)
             running = self.dispatch_events()
             self.controller.process()
@@ -363,12 +364,12 @@ CONFIG = {'width': 900,
           'backcol': (250, 250, 250),
           'robot_col': (0, 99, 199),
           'goal_col': (255, 0, 0),
-          'fps': 200,       # Pygame clock ticks
-          'dt': 0.005,      # samples per second
+          'fps': 40,       # Pygame clock ticks
+          'dt': 0.01,       # Time delta per frame duration
           'pi_step': 4,     # PI domain: angle = PI * 2 / pi_step
-          'pi_eps': 80,     # PI domain: angle epsilon = PI * 2 / pi_eps
+          'pi_eps': 100,     # PI domain: angle epsilon = PI * 2 / pi_eps
           'move_step': 400, # screen domain
-          'move_eps': 30}   # screen domain (goal proximity)
+          'move_eps': 20}   # screen domain (goal proximity)
 
 ####
 
